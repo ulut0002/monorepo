a3f1f64e7d44e67deac07ed5c5e6fee0f20987a2
serdarulutas <ulut0002@algonquinlive.com>
2025-06-06 00:13:02 -0400

    chore: update package.json dependencies and refactor tsconfig.json for consistency

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,10 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster richColors />
         {children}
       </body>
     </html>
